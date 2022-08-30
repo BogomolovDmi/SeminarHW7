415eacdbddaabf96f51ce66219b82dde83727187
DmitryB <BogomolovDME@yandex.ru>
2022-08-30 13:51:05 +0300

    first commit

---
+++ Task050/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    var random = new Random();
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.Next(1, 100);
+        }
+    }
+    return result;
+}
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+    Console.WriteLine("Всё плохо");
+}
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+    Console.WriteLine("Всё плохо");
+}
+var array = CreateArrayWithRandomNumbers(m, n);
+PrintArray(array);
+
+Console.WriteLine("Введите индекс строки искомого числа");
+int line = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите индекс столбца искомого числа");
+int column = Convert.ToInt32(Console.ReadLine());
+
+if (line > m || column > n)
+{
+    Console.WriteLine("такого числа в массиве нет");
+}
+else
+{
+    object number = array.GetValue(line, column);
+    Console.WriteLine("Искомое число = " + number);
+}
---
+++ Task052/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++) Console.Write($"{array[i, j]}, ");
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] result = new int[m, n];
+    var random = new Random();
+    for (var i = 0; i < result.GetLength(0); i++)
+        for (var j = 0; j < result.GetLength(1); j++)
+            result[i, j] = random.Next(1, 10);
+    return result;
+}
+void Average(int[,] array)
+{
+    int i = 0;
+    int j = 0;
+    for (j = 0; j < array.GetLength(1); j++)
+    {   
+        
+        double averagesum = 0;
+        double sum = 0;
+        for (i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        averagesum = Math.Round((sum / array.GetLength(0)), 3);
+        
+        Console.WriteLine($"Среднее арифметическое столбца ({j + 1}) = {sum} / {array.GetLength(0)} = {averagesum}");
+        
+    }
+    Console.WriteLine(String.Empty);
+}
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m)) Console.WriteLine("Всё плохо");
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n)) Console.WriteLine("Всё плохо");
+
+int[,] array = CreateArrayWithRandomNumbers(m, n);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+Average(array);
